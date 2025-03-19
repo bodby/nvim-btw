13d3f0472ae8195989d844d557b0fcdc77caa50c
Baraa Homsi <baraa.homsi@proton.me>
2025-03-19 13:47:11 +0000

    fix(folds): finally got folds to work

--- nvim/lua/bodby/native/folds.lua
+++ nvim/lua/bodby/native/folds.lua
@@ -1,19 +1,12 @@
-local trim = require("bodby.shared").trim
+local trim = require('bodby.shared').trim
 
 local M = { }
 
 function M.setup()
-  vim.o.foldmethod = "expr"
-  vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-  vim.o.foldtext = "v:lua.require('bodby.native.folds').text()"
+  vim.o.foldmethod = 'expr'
+  vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+  vim.o.foldtext = 'v:lua.require("bodby.native.folds").text()'
 end
-
--- FIXME: I wrote the two functions below on mobile. I think there's an
---        off-by-one error in 'highlight_line()'. Or they just don't work
---        entirely.
---
---        The return type annotation on 'highlight_line()' and 'text()' don't
---         make sense.
 
 --- Return the last Treesitter capture at a position or 'Folded' if none exist.
 ---
@@ -32,25 +25,23 @@ local function get_highlight(buffer, row, col)
 end
 
 --- Return a list of pairs of text to highlight and their highlight.
---- Directly usable in 'foldtext' if `vim.v.foldstart` or `foldend` is passed.
+--- Directly usable in 'foldtext' if `v:foldstart` or `v:foldend` is passed.
 ---
 --- @param buffer integer
 --- @param row integer
 --- @return table<string>[]
 local function highlight_line(buffer, row)
   local result = { }
-  local prev = get_highlight(buffer, row, 0)
+  local prev = get_highlight(buffer, row - 1, 0)
   local offset = 1
 
   local line = vim.api.nvim_buf_get_lines(buffer, row - 1, row, true)[1]
   local length = #line
   for i = 1, length do
-    local highlight = get_highlight(buffer, row, i - 1)
+    local highlight = get_highlight(buffer, row - 1, i)
     if highlight ~= prev then
       table.insert(result, { line:sub(offset, i), prev })
-      offset = i 
-    else if i == length then
-      table.insert(result, { line:sub(-1), prev })
+      offset = i + 1
     end
     prev = highlight
   end
@@ -59,17 +50,22 @@ end
 
 --- Expression used in 'foldtext'.
 ---
---- @return table<string, string>
+--- @return table<string>[]
 function M.text()
   -- TODO: Make this per-buffer by passing the window (same with winbar).
-  
-  return highlight_line(vim.api.nvim_get_current_buf(), vim.v.foldstart)
+  local foldstart = highlight_line(vim.api.nvim_get_current_buf(), vim.v.foldstart)
+  local result = foldstart
+  table.insert(result, { ' ... ', 'Folded' })
+  local foldend = highlight_line(vim.api.nvim_get_current_buf(), vim.v.foldend)
+
+  for _, v in ipairs(foldend) do
+    local trimmed = v
+    trimmed[1] = trim(trimmed[1])
+    table.insert(result, trimmed)
+  end
+
   -- Highlights can be added if a table is passed instead of just a string.
-  -- return {
-  --   { line, "Folded" },
-  --   { " ... ", "Delimiter" },
-  --   { trim(delimiter), "Folded" }
-  -- }
+  return result
 end
 
 return M
